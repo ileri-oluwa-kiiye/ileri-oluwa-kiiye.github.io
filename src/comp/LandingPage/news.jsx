c95aa88b82e4f77cb1fd5afc1de337fd93fbf178
ileri-oluwa-kiiye <aboladeilerioluwakiiye@gmail.com>
2025-11-06 10:57:43 +0100

    news adding links

--- src/comp/LandingPage/news.jsx
+++ src/comp/LandingPage/news.jsx
@@ -1,188 +1,178 @@
 import { NewsSection } from "../../assets/styles";
 
 const newsItems = [
-  
   {
     id: 1,
     date: "Nov 2025",
     description: (
       <>
-        I will be giving my talk &quot;Why Your Medical AI Model Might Not Work in Africa: A Python Guide to Measuring Bias&quot; again at PyLadiesCon 2025.
+        I will be giving my talk &quot;Why Your Medical AI Model Might Not Work in Africa: A Python Guide to Measuring Bias&quot; again at 
+        <a href="https://2025.conference.pyladies.com/en/" target="_blank" rel="noopener noreferrer"> PyLadiesCon 2025</a>.
       </>
     ),
-    // link: { text: "PyLadiesCon 2025", url: "https://example.com/pyladiescon" }
   },
   {
     id: 2,
     date: "Oct 2025",
     description: (
       <>
-        I led Team Mamalens, and we were a finalist at the Data Science for Health Ideathon, organized in Collaboration with the Deep Learning Indaba. We also won the most fun project award!
+        I led Team Mamalens, and we were a finalist at the Data Science for Health Ideathon (in collaboration with the 
+        <a href="https://deeplearningindaba.com/" target="_blank" rel="noopener noreferrer"> Deep Learning Indaba</a>). We also won the “most fun project” award!  
+        <a href="https://docs.google.com/presentation/d/17LVUDp3HcsDdlQ2ZxIaimRf1jW5snWvd-r6mWReY9Eo/edit?usp=sharing" target="_blank" rel="noopener noreferrer"> Link to Slides.</a>
       </>
     ),
-    // link: { text: "AMTES, FUNAAB", url: "https://example.com/amtes" }
-  },
-  {
-    id: 2,
-    date: "Oct 2025",
-    description: (
-      <>
-        I was invited as a guest speaker to speak on &quot;Making Impact Beyond School&quot; at the Freshers Orientation of the Association of Mechatronics Engineering Students. Thank you to the executives!
-      </>
-    ),
-    // link: { text: "AMTES, FUNAAB", url: "https://example.com/amtes" }
   },
   {
     id: 3,
     date: "Oct 2025",
     description: (
       <>
-        Delivered a talk on &quot;Why Your Medical AI Model Might Not Work in Africa: A Python Guide to Measuring Bias&quot; at PyCon Nigeria 2025.
+        I was invited as a guest speaker to speak on  <a href="https://docs.google.com/presentation/d/1o1sqlk28dzbdeLgcwLCnaqV-XjCXnD8NrVayj5IjQhw/edit?usp=sharing" target="_blank" rel="noopener noreferrer">&quot;Making Impact Beyond School&quot;  </a>  at the Freshers Orientation of the 
+        Association of Mechatronics Engineering Students (AMTES), FUNAAB. Thank you to the executives!
       </>
     ),
-    // link: { text: "PyCon Nigeria 2025", url: "https://example.com/pycon-nigeria" }
   },
   {
     id: 4,
     date: "Oct 2025",
     description: (
       <>
-        I have just been appointed as Co-Lead 2 of Google Developer Groups FUNAAB, after serving as ML Co-Lead for a year.{' '}
-        <a
-          href="https://chat.whatsapp.com/IHdzT3JF5B4IgNq4u6BNKU"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="text-blue-500 hover:underline"
-        >
-          Join our community here!
-        </a>
+        Delivered a talk on &quot;Why Your Medical AI Model Might Not Work in Africa: A Python Guide to Measuring Bias&quot; at 
+        <a href="https://ng.pycon.org/" target="_blank" rel="noopener noreferrer"> PyCon Nigeria 2025</a>.  
+        <a href="https://docs.google.com/presentation/d/1FQYHi3cvTZKPZ8NSkW646vOLWlyKP7ZKYIKjU2yCRl4/edit?usp=sharing" target="_blank" rel="noopener noreferrer"> Link to Slides.</a>
       </>
-    )
+    ),
   },
   {
     id: 5,
-    date: "Sept 2025",
+    date: "Oct 2025",
     description: (
       <>
-        Presented &quot;SharpXR: Structure-Aware Denoising for Pediatric Chest X-Rays&quot; at the MICCAI MIRASOL Workshop in Daejeon, South Korea — awarded Best Paper Runner-Up!
+        I have just been appointed as Co-Lead of the 
+        <a href="https://gdg.community.dev/gdg-on-campus-federal-university-of-agriculture-abeokuta-nigeria/" target="_blank" rel="noopener noreferrer"> Google Developer Groups FUNAAB</a>, after serving as ML Co-Lead for a year.{' '}
+        <a href="https://chat.whatsapp.com/IHdzT3JF5B4IgNq4u6BNKU" target="_blank" rel="noopener noreferrer" className="text-blue-500 hover:underline">Join our community here!</a>
       </>
-    ),
-    // link: { text: "MICCAI 2025", url: "https://example.com/miccai2025" }
+    )
   },
   {
     id: 6,
     date: "Sept 2025",
     description: (
       <>
-        Our paper &quot;Addressing Domain Shift in Low-Resource Neuroimaging: Glioma Segmentation for Sub-Saharan MRI&quot; was accepted to WiML @ NeurIPS 2025. Awarded a full travel grant to attend.
+        Presented &quot;SharpXR: Structure-Aware Denoising for Pediatric Chest X-Rays&quot;  at the MIRASOL Workshop, 
+         — <b>  awarded Best Paper Runner-Up!</b>
       </>
     ),
-    // link: { text: "WiML @ NeurIPS 2025", url: "https://example.com/wiml-neurips" }
   },
   {
     id: 7,
-    date: "August 2025",
+    date: "Sept 2025",
     description: (
       <>
-        Won a Best Poster Award at Deep Learning Indaba 2025 for &quot;Fairness-Aware Machine Learning for Social Bias Detection in Healthcare Research Datasets.&quot; which I presented as a collaborator.
+        Our paper &quot;Addressing Domain Shift in Low-Resource Neuroimaging: Glioma Segmentation for Sub-Saharan MRI&quot; was accepted to 
+        <a href="https://sites.google.com/wimlworkshop.org/wimlworkshopneurips2025/home" target="_blank" rel="noopener noreferrer"> WiML @ NeurIPS 2025</a>. Awarded a full travel grant to attend.
       </>
     ),
-    // link: { text: "Deep Learning Indaba 2025", url: "https://example.com/indaba2025" }
   },
   {
     id: 8,
-    date: "June 2025",
+    date: "Aug 2025",
     description: (
       <>
-        My first paper &quot;SharpXR: Structure-Aware Denoising for Pediatric Chest X-Rays&quot; was accepted at the MICCAI MIRASOL Workshop! I led this research as the lead for the ML Collective medical imaging focus group in Nigeria. Congratulations to the team!
+        Won a Best Poster Award at the 
+        <a href="https://deeplearningindaba.com/" target="_blank" rel="noopener noreferrer"> Deep Learning Indaba 2025</a> for  
+        &quot;Fairness-Aware Machine Learning for Social Bias Detection in Healthcare Research Datasets.&quot; which I presented as a collaborator. <a href="https://drive.google.com/file/d/1H2FJLdhIbC-CqsdtXp1H5R2d35ZRlnPq/view?usp=sharing" target="_blank" rel="noopener noreferrer"> Link to Poster </a>
       </>
     ),
-    // link: { text: "MIRASOL Workshop", url: "https://example.com/mirasol" }
   },
   {
     id: 9,
-    date: "Mar 2025",
+    date: "June 2025",
     description: (
       <>
-        Excited to have been selected for the MICCAI Society Mentorship Program and be paired with Dr. Qingjie Meng!
+        My first paper &quot;SharpXR: Structure-Aware Denoising for Pediatric Chest X-Rays&quot; was accepted at the <a href="https://event.fourwaves.com/mirasol/pages" target="_blank" rel="noopener noreferrer"> MICCAI MIRASOL Workshop!</a> I led this research as the lead for the 
+        <a href="https://mlcollective.org" target="_blank" rel="noopener noreferrer"> ML Collective</a> medical imaging focus group in Nigeria. Congratulations to the team!
       </>
     ),
   },
-  // {
-  //   id: 10,
-  //   date: "Feb 2025",
-  //   description: "Awarded a $2000 travel grant to attend PyCon US 2025, the largest annual convention for the Python programming language.",
-  //   // link: { text: "PyCon US 2025", url: "https://example.com/pyconus2025" }
-  // },
+  {
+    id: 10,
+    date: "Mar 2025",
+    description: (
+      <>
+        Excited to have been selected for the 
+        <a href="https://miccai.org/index.php/about-miccai/miccai-society-mentorship-program/" target="_blank" rel="noopener noreferrer"> MICCAI Society Mentorship Program</a> and be paired with <a href="https://qmeng99.github.io//" target="_blank" rel="noopener noreferrer"> Prof. Qingjie Meng!</a>
+      </>
+    ),
+  },
   {
     id: 11,
     date: "Sept 2024",
     description: (
       <>
-        Presented a poster on Maternify, at Deep Learning Indaba 2024 in Dakar, Senegal. Thank you to the DLI Committee for the full sponsorship.
+        Presented a poster on “Maternify” at the 
+        <a href="https://deeplearningindaba.com/" target="_blank" rel="noopener noreferrer">Deep Learning Indaba 2024</a> in Dakar, Senegal. Thank you to the DLI Committee for the full sponsorship.
       </>
     ),
-    // link: { text: "Deep Learning Indaba 2024", url: "https://example.com/indaba2024" }
   },
   {
     id: 12,
     date: "Sept 2024",
     description: (
       <>
-        I spoke at DjangoCon US 2024 on &quot;The Magic of Dependencies Installing Themselves,&quot; highlighting PEP 723 for reproducibility and efficiency.
+        I spoke at 
+        <a href="https://2024.djangocon.us/" target="_blank" rel="noopener noreferrer"> DjangoCon US 2024</a> on &quot;The Magic of Dependencies Installing Themselves,&quot; highlighting PEP 723 for reproducibility and efficiency.  
+        <a href="https://pretalx.com/djangocon-us-2024/talk/8TBXVX/" target="_blank" rel="noopener noreferrer"> Link to my talk.</a>
       </>
     ),
-    // link: { text: "DjangoCon US 2024", url: "https://example.com/djangocon2024" }
   },
   {
     id: 13,
     date: "Sept 2024",
     description: (
       <>
-        I will be at Accra, Ghana for Pycon Africa 2024! Thank you to the python community for the full sponsorship.
+        I will be at 
+        <a href="hhttps://africa.pycon.org/2024/" target="_blank" rel="noopener noreferrer"> PyCon Africa 2024</a> in Accra, Ghana! Thank you to the Python community for the full sponsorship.
       </>
     ),
-    // link: { text: "PyCon Africa 2024", url: "https://example.com/pyconafrica2024" }
   },
   {
     id: 14,
     date: "June 2024",
     description: (
       <>
-        Earned the &quot;Microsoft Azure Responsible AI Workshop Coach&quot; badge for mentoring participants on building fair and trustworthy AI systems.
+        Earned the “Microsoft Azure Responsible AI Workshop Coach” badge for mentoring participants on building fair and trustworthy AI systems.
       </>
     ),
-    // link: { text: "Microsoft Azure Responsible AI Workshop", url: "https://example.com/azure-ai-workshop" }
   },
   {
     id: 15,
     date: "May 2024",
     description: (
       <>
-        I built Maternify, an ML-powered prenatal care chatbot, with Toyibat Adele, Kelechi Chbundu and Muslimah Sarumi, and we ranked Top 5 in the Microsoft Ambassador&rsquo;s AI Challenge.
+        I built “Maternify,” an ML-powered prenatal care chatbot, with Toyibat Adele, Kelechi Chbundu and Muslimah Sarumi, and we ranked Top 5 in the “Microsoft Ambassador’s AI Challenge.”
       </>
     ),
-    // link: { text: "Microsoft Ambassador’s AI Challenge", url: "https://example.com/microsoft-ai-challenge" }
   },
   {
     id: 16,
     date: "Feb 2024",
     description: (
       <>
-        Received a $2000 grant to attend PyCon US 2024, the largest annual convention for the Python programming language.
+        Received a $2,000 grant to attend 
+        <a href="hhttps://us.pycon.org/2024/" target="_blank" rel="noopener noreferrer">PyCon US 2024</a>, the largest annual convention for the Python programming language.
       </>
     ),
-      // link: { text: "PyCon US 2024", url: "https://example.com/pyconus2024" }
   },
   {
     id: 17,
     date: "Jan 2024",
     description: (
       <>
-        Delivered a talk on &quot;Balancing Academics and a Thriving Tech Career&quot; at the Assosiation of Mechatronics Engineering Students&#39; Freshers Orientation (AMTES, FUNAAB).
+        Delivered a talk on &quot;Balancing Academics and a Thriving Tech Career&quot; at the 
+        AMTES (FUNAAB) Freshers Orientation
       </>
     ),
-    // link: { text: "AMTES, FUNAAB", url: "https://example.com/amtes" }
   }
 ];
 
